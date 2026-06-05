d5dc079b7fe9d2a1c1c94f80b1b0350b33d4bdd7
conghan <conghan.cai@gmail.com>
2026-06-05 16:02:52 +0800

    utils2

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ import { addRoute, startRouter } from './router.js';
 import { render as homeRender, afterRender as homeAfter } from './pages/home.js';
 import { render as aboutRender, afterRender as aboutAfter } from './pages/about.js';
 import { render as productsRender, afterRender as productsAfter } from './pages/products.js';
+import { render as utilsRender, afterRender as utilsAfter } from './pages/utils.js';
 
 addRoute('/', homeRender, homeAfter);
 addRoute('/about', aboutRender, aboutAfter);
