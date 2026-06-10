f5fab9446921c90c7beb91ba2abc4243a26d6584
conghan <conghan.cai@gmail.com>
2026-06-10 19:59:20 +0800

    add manifest

--- public/js/pages/about.js
+++ public/js/pages/about.js
@@ -1,19 +1,37 @@
-function style(){
-    return `
-	<style>
-		p { font-size: 20px; background-color: red;}
-	</style>
-    `;
+function style() {
+  return /* HTML */ `<style>
+    p {
+      font-size: 20px;
+      background-color: red;
+    }
+    .cards {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 10px;
+    }
+  </style>`;
 }
+
+function card() {
+  return /* HTML */ `<div>
+    <img
+      src="https://placehold.co/100x100"
+      width="100"
+      height="100"
+      alt="grey"
+    />
+  </div>`;
+}
+
 export function render() {
-    return `
-    	${style()}
-        <h1>关于</h1>
-        <p>原生 SPA 脚手架，理解 Vue/React 路由底层原理。</p>
-        <p>页面切换完全通过 Hash 路由实现，不刷新浏览器。</p>
-    `;
+  const n = Math.floor(Math.random() * 100) + 1;
+  const cards = Array.from({ length: n }, () => card()).join("");
+  return /* HTML */ `
+    ${style()}
+    <div class="cards">${cards}</div>
+  `;
 }
 
 export function afterRender() {
-    // 暂无交互
+  // 暂无交互
 }
---
+++ test.js
@@ -0,0 +1,11 @@
+function card() {
+  return /* HTML */ `<div>
+    <h1>hello</h1>
+    <p>yes my</p>
+  </div>`;
+}
+
+const a = /* HTML */ `<div>
+  <h1>hello</h1>
+  <p>yes my</p>
+</div>`;
